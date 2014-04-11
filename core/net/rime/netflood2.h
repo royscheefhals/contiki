228b3ce5f3d482327a946e2e201afc43066079be
Roy Scheefhals <Royscheefhals@hotmail.com>
2014-04-11 10:59:59 +0200

    Improved the overflow bit detection for the following situation:
       overflow bit seq_no
    1      1	127
    2      0	0  ( newest message )
    3      1	127
    
    This situation gets filtered.
    This will however result in a network failure of 256 messages when a node misses 128 consecutive messages. more info in netflood2.h

--- core/net/rime/netflood2.h
+++ core/net/rime/netflood2.h
@@ -4,10 +4,10 @@
  */
 
 /**
- * \defgroup rimenetflood Best-effort network flooding
+ * \defgroup rimenetflood Best-effort network flooding version 2
  * @{
  *
- * The netflood module does best-effort flooding.
+ * The netfloodv2 module does best-effort flooding.
  *
  * The netflood primitive sends a single packet to all nodes in the
  * network. The netflood primitive uses polite broadcasts at every hop
@@ -15,21 +15,30 @@
  * primitive does not perform retransmissions of flooded packets and
  * packets are not tagged with version numbers.  Instead, the netflood
  * primitive sets the end-to-end sender and end-to-end packet ID
- * attributes on the packets it sends.  A forwarding node saves the
- * end-to-end sender and packet ID of the last packet it forwards and
+ * attributes on the packets it sends.  A forwarding node saves a set 
+ * number of end-to-end sender and packet ID packets it forwards and
  * does not forward a packet if it has the same end-to-end sender and
  * packet ID as the last packet.  This reduces the risk of routing
  * loops, but does not eliminate them entirely as the netflood
- * primitive saves the attributes of the latest packet seen only.
- * Therefore, the netflood primitive also uses the time to live
- * attribute, which is decreased by one before forwarding a packet.
- * If the time to live reaches zero, the primitive does not forward
- * the packet.
+ * primitive saves the attributes a set amount of latest packets
+ * seen only. Therefore, the netflood primitive also uses the time 
+ * to live attribute, which is decreased by one before forwarding a
+ * packet. If the time to live reaches zero, the primitive does not
+ * forward the packet. 
+ *  
+ * Version 2 of the netflood primitive will manage the ID numbers set
+ * on packets itself. Version 1 didn't check for overflowing ID numbers
+ * and would result in a network failure at maximum sequence number for
+ * one particular node. Version 2 will detect this up unti a certain 
+ * point. When the receiving node misses 128 consecutive packets, it will 
+ * result in a network failure for the next 256 packets for the corresponding 
+ * sender. This can be limited by using timing ( long time no packet -> 
+ * reset ID numbers), asking other nodes after multiple failures, or by
+ * using more bytes for the ID-number.
 *
  * \section channels Channels
  *
- * The netflood module uses 2 channels. One for the data transmissions.
- * And one for the neighbour detection and broadcast
+ * The netflood module uses 1 channel
  *
  */
 
